fc93ce7dc60143b57bddf509b4c1a1e94a3d6622
yannmarc <chamackstone@gmail.com>
2023-04-27 18:11:53 +0100

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path='/' Component={Home} />
                 <Route path='/login' Component={Login} />
                 <Route path='/signup' Component={SignUp} />
+                <Route path='/dashboard' Component={Dashboard} />
             </Routes>
         </BrowserRouter>
     </Fragment>
