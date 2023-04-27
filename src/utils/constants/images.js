9c85d21a3e062eacad18fb4177db07f49b1a225c
yannmarc <chamackstone@gmail.com>
2023-04-27 18:11:12 +0100

    added images

--- src/utils/constants/images.js
+++ src/utils/constants/images.js
@@ -2,10 +2,14 @@ import logo from '../../assets/imgs/logo.svg';
 import headerImg from '../../assets/imgs/header-img.jpg';
 import avatar from '../../assets/imgs/avatar.jpg';
 import painter from '../../assets/imgs/painter.jpg';
+import iconMoon from '../../assets/imgs/icon-moon.svg';
+import iconSun from '../../assets/imgs/icon-sun.svg';
 
 export {
     logo,
     headerImg,
     avatar,
-    painter
+    painter,
+    iconMoon,
+    iconSun
 }
