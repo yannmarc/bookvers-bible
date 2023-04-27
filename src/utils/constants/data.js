4c19038c87d04fa02ab5e920552ba9607a27a348
yannmarc <chamackstone@gmail.com>
2023-04-27 18:10:48 +0100

    added constand data

--- src/utils/constants/data.js
+++ src/utils/constants/data.js
@@ -16,6 +16,92 @@ const featureText = [
     }
 ];
 
+const navItems = [
+    {
+      id: 1,
+      text: "Old testament",
+    },
+    {
+      id: 2,
+      text: "New testament"
+    },
+    {
+      id: 3,
+      text: "Bookmark",
+      link: "bookmarks"
+    },
+    {
+      id: 4, 
+      text: "My Notes",
+      link: "mynotes"
+    }
+  ];
+
+  const bibleVerse = [
+    {
+      id: 1, 
+      book: "Genesis",
+      chapCount: 40
+    },
+    {
+      id: 2, 
+      book: "Exodus",
+      chapCount: 24
+    },
+    {
+      id: 3, 
+      book: "Leviticus",
+      chapCount: 15
+    },
+    {
+      id: 4, 
+      book: "Numbers",
+      chapCount: 20
+    },
+    {
+      id: 5, 
+      book: "Detoronomy",
+      chapCount: 35
+    },
+    {
+      id: 6, 
+      book: "Joshua",
+      chapCount: 10
+    },
+    {
+      id: 7, 
+      book: "Judges",
+      chapCount: 8
+    },
+    {
+      id: 8, 
+      book: "Ruth",
+      chapCount: 40
+    },
+    {
+      id: 9, 
+      book: "1 Samuel",
+      chapCount: 59
+    },
+    {
+      id: 10, 
+      book: "2 Samuel",
+      chapCount: 60
+    },
+    {
+      id: 11, 
+      book: "2 Samuel",
+      chapCount: 60
+    },
+    {
+      id: 12, 
+      book: "2 Samuel",
+      chapCount: 60
+    },
+  ]
+
 export { 
-    featureText
+    featureText,
+    navItems,
+    bibleVerse
 }
