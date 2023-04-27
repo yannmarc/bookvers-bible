1f4bebebceaaade935dd554924d2ee77f3d81dce
yannmarc <chamackstone@gmail.com>
2023-04-27 07:21:24 +0100

    adding utility files

---
+++ src/utils/constants/data.js
@@ -0,0 +1,21 @@
+const featureText = [
+    {
+        id: 1,
+        heading: "Customized interface",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eum vitae quod rem esse, nemo omnis dolores ea."
+    },
+    {
+        id: 2,
+        heading: "Note taking",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eum vitae quod rem esse, nemo omnis dolores ea."
+    },
+    {
+        id: 3,
+        heading: "Custom bookmarks",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eum vitae quod rem esse, nemo omnis dolores ea."
+    }
+];
+
+export { 
+    featureText
+}
---
+++ src/utils/constants/images.js
@@ -0,0 +1,11 @@
+import logo from '../../assets/imgs/logo.svg';
+import headerImg from '../../assets/imgs/header-img.jpg';
+import avatar from '../../assets/imgs/avatar.jpg';
+import painter from '../../assets/imgs/painter.jpg';
+
+export {
+    logo,
+    headerImg,
+    avatar,
+    painter
+}
